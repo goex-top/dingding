ceb81c6fdc091ba2b96cc99682f1532173597498
eric <btstarinfo@gmail.com>
2020-04-04 11:42:04 +0800

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/beaquant/dingding
+
+go 1.14
+
+require (
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0
+)
---
+++ message.go
@@ -0,0 +1,36 @@
+package dingding
+
+// 详情参见 https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq
+
+const (
+	MsgTypeText     = "text"     //text 类型
+	MsgTypeLink     = "link"     //link 类型
+	MsgTypeMarkdown = "markdown" //markdown 类型
+)
+
+// Message 普通消息
+type Message struct {
+	Content  string `validate:"required"`
+	AtPerson []string
+	AtAll    bool
+}
+
+// Link 链接消息
+type Link struct {
+	Content    string `json:"text" validate:"required"`       // 要发送的消息， 必填
+	Title      string `json:"title" validate:"required"`      // 标题， 必填
+	ContentURL string `json:"messageUrl" validate:"required"` // 点击消息跳转的URL 必填
+	PictureURL string `json:"picUrl"`                         // 图片 url
+}
+
+// Markdown markdown 类型
+type Markdown struct {
+	Content string `json:"text" validate:"required"`  // 要发送的消息， 必填
+	Title   string `json:"title" validate:"required"` // 标题， 必填
+}
+
+// SimpleMessage push message
+type SimpleMessage struct {
+	Content string
+	Title   string
+}
